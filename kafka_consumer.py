831bbbda60fbe7648511035327b8e802f20956d8
Hessel Tjeerdsma <hessel@tjeerdsma.eu>
2024-09-11 15:28:40 +0200

    feat: Add new Kafka subscribers for fraud, hate speech, and fake news detection

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -4,13 +4,16 @@ from faststream import FastStream
 broker = KafkaBroker("localhost:29092")
 app = FastStream(broker)
 
+
 @broker.subscriber("fraud-detection")
 async def process_fraud_detection(msg: KafkaMessage):
     print(f"Fraud Detection: {msg.value}")
 
+
 @broker.subscriber("hatespeech-detection")
 async def process_hatespeech_detection(msg: KafkaMessage):
-    print(f"Hate Speech Detection: {msg.value}")
+    print(f"Hate Speech Detection: {msg}")
+
 
 @broker.subscriber("fakenews-detection")
 async def process_fakenews_detection(msg: KafkaMessage):
