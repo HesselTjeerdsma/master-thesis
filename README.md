5dac0ce084be0c121b7060a2585d1eccd579a0ab
Hessel Tjeerdsma <hessel@tjeerdsma.eu>
2024-09-13 12:05:25 +0200

    feat: add publisher for fraud data

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # master-thesis
 This is  the codebase for my master thesis at the JADS.
+
+
+## Datasets
+https://www.kaggle.com/datasets/kartik2112/fraud-detection?resource=download
--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,24 +1,20 @@
 from faststream.kafka import KafkaBroker, KafkaMessage
 from faststream import FastStream
 
-def create_app():
-    broker = KafkaBroker("kafka:9092")
-    app = FastStream(broker)
+broker = KafkaBroker("localhost:29092")
+app = FastStream(broker)
 
-    @broker.subscriber("fraud-detection")
-    async def process_fraud_detection(msg: KafkaMessage):
-        print(f"Fraud Detection: {msg.value}")
 
-    @broker.subscriber("hatespeech-detection")
-    async def process_hatespeech_detection(msg: KafkaMessage):
-        print(f"Hate Speech Detection: {msg.value}")
+@broker.subscriber("fraud-detection")
+async def process_fraud_detection(msg: KafkaMessage):
+    print(f"Fraud Detection: {msg.value}")
 
-    @broker.subscriber("fakenews-detection")
-    async def process_fakenews_detection(msg: KafkaMessage):
-        print(f"Fake News Detection: {msg.value}")
 
-    return app
+@broker.subscriber("hatespeech-detection")
+async def process_hatespeech_detection(msg: KafkaMessage):
+    print(f"Hate Speech Detection: {msg}")
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run()
+
+@broker.subscriber("fakenews-detection")
+async def process_fakenews_detection(msg: KafkaMessage):
+    print(f"Fake News Detection: {msg.value}")
